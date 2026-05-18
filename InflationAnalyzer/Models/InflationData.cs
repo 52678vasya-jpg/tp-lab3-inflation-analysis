abf4616829a7210a4d6c7090e5bfd6ddc303c4c8
player404 <52678vasya@gmail.com>
2026-05-19 01:44:09 +0300

    fix styles

--- InflationAnalyzer/Models/InflationData.cs
+++ InflationAnalyzer/Models/InflationData.cs
@@ -2,7 +2,13 @@
 {
     public class InflationData
     {
+        // Год
         public int Year { get; set; }
+
+        // Инфляция
         public double Inflation { get; set; }
+
+        // Цена квартиры
+        public double ApartmentPrice { get; set; }
     }
 }
